3632049eaba38230c30ad0242e7a8e938cc56da4
David <dvdlee41@gmail.com>
2020-11-07 23:19:23 +0800

    Adding Item Model

---
+++ ExpandingStackCells/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  ExpandingStackCells
+//
+//  Created by David on 2020/11/7.
+//  Copyright © 2020 David. All rights reserved.
+//
+
+import Foundation
+
+struct Item {
+  let title: String
+  let detail: String
+}
